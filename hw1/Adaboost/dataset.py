0a9116388a6683c17573c38cfed21a100381fbd5
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-03-15 11:57:59 +0800

    hw1 files

---
+++ hw1/Adaboost/dataset.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+
+def loadImages(dataPath):
+    """
+    Load all Images in the folder and transfer a list of tuples. 
+    The first element is the numpy array of shape (m, n) representing the image.
+    (remember to resize and convert the parking space images to 36 x 16 grayscale images.) 
+    The second element is its classification (1 or 0)
+      Parameters:
+        dataPath: The folder path.
+      Returns:
+        dataset: The list of tuples.
+    """
+    # Begin your code (Part 1)
+    raise NotImplementedError("To be implemented")
+    # End your code (Part 1)
+    
+    return dataset
